41f644dd1432dfc65cdaac8d73abc562b5716ae3
Manu <manuklmanu535@gmail.com>
2023-11-14 17:54:03 +0530

    no changes

--- login.php
+++ login.php
@@ -7,6 +7,7 @@ function function_alert($message, $redirectUrl) {
     // Redirect to the specified URL after the alert is closed
     echo "<script>window.location.href = '$redirectUrl';</script>";
 }
+
 $host = "localhost";
 $username = "root";
 $password = "";
@@ -21,26 +22,23 @@ if ($conn->connect_error) {
 $enteredUsername = $_POST['username'];
 $enteredPassword = $_POST['password'];
 
-$query = "SELECT * FROM users WHERE username='$enteredUsername' AND password='$enteredPassword'";
-
-$result = $conn->query($query);
+// Use prepared statements to prevent SQL injection
+$query = "SELECT * FROM users WHERE username=? AND password=?";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("ss", $enteredUsername, $enteredPassword);
+$stmt->execute();
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-    echo "Login succesfull";
- 
-        // Verify the entered password against the hashed password in the database
-        //if ($enteredPassword ==='username' &&  $enteredPassword ==='password') {
-            // Authentication successful, redirect to index page
-            header("Location:index.html");
-            exit;   
-        }
- //}
-        else {
-        $message = "Invalid Username or Password";
-        $redirectUrl = "login.html";
-        function_alert($message, $redirectUrl);
+    // Authentication successful, redirect to index page
+    header("Location: index.html");
+    exit;
+} else {
+    $message = "Invalid Username or Password";
+    $redirectUrl = "login.html";
+    function_alert($message, $redirectUrl);
 }
 
-
+$stmt->close();
 $conn->close();
 ?>
