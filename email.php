48d7c7a8beaef86580b35763c521b0bda82cea71
Manu <manuklmanu535@gmail.com>
2023-11-30 15:07:58 +0530

    added css effect

---
+++ email.php
@@ -0,0 +1,62 @@
+<?php
+ function function_alert($message, $redirectUrl) {
+    // Display the alert box
+    echo "<script>alert('$message');</script>";
+    // Redirect to the specified URL after the alert is closed
+    echo "<script>window.location.href = '$redirectUrl';</script>";
+}
+$email = $_POST['email'];
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "usersdb";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $regemail = $_POST['email'];
+    // $regPassword = $_POST['password'];
+
+ // Check if the username is available
+ $checkQuery = "SELECT * FROM email WHERE email = '$regemail'";
+ $result = $conn->query($checkQuery);
+ if ($result->num_rows > 0) {
+     $message = "Email already used";
+     $redirectUrl = "index.html";
+     function_alert($message, $redirectUrl);
+ }
+else{
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    // Insert new user into the database
+    $insertQuery = "INSERT INTO email VALUES ('$regemail')";
+    if ($conn->query($insertQuery) === TRUE) {
+        
+        //echo "Registration successful! Kindly Go Back & Login With Your Credentials";
+       
+        $message = "Thanks for interest";
+        $redirectUrl = "index.html";
+        function_alert($message, $redirectUrl);
+  }
+}
+else
+{
+    $message = "Invalid Email";
+    $redirectUrl = "index.html";
+    function_alert($message, $redirectUrl);
+}
+}
+}
+ else {
+        //Future Use with appropriate message
+    } 
+$conn->close();
+?>
+
+
+
+
+
