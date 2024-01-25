15295674182c4ee0ee040dd0bf0a88eaba9e9076
Manukl535 <manuklmanu535@gmail.com>
2024-01-25 18:13:37 +0530

    CHANGES TO A/C SECTION

--- account.php
+++ account.php
@@ -11,7 +11,6 @@ if(!isset($_SESSION['logged-in'])){
 <html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
- 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="styles.css">
   <title></title>
